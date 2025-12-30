181818d557cfe13c88dccc31928ad2134723837b
rootearch <113116099+rootearch@users.noreply.github.com>
2025-12-30 10:33:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,3 +8,18 @@ testtoken
 
 aws_test_access_key_id = AKIA34BAGDIHTV5BO7P3
 aws_secret_test_access_key = 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
+
+"email": "s1111(at)gmail[.]com",
+"phone": "9090909012"
+}",
+"descriptionFormat": null,
+"description": null,
+"variable ... {
+"key": "password",
+"value": "admin123",
+"type": "string"
+
+"requestObject": "{"id":"46ae1ab2-b8b0-4064-8bed-835275a7a1eb","method":"GET","headers":"authorization: Bearer <token>","pathVariables":{"BusinessID":"5640"},"url":"{{baseUrl}}/api/Reports/GetSalesReportsAndStats/:BusinessID","pathVariableData":[{"key":"BusinessID
+
+"email": "bsXSn4(at)kKyJwTrCZnuKDGTjhFmPRcGiQ[.]xu",
+"password": "&x6UCXos8y"
